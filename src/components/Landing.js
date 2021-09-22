08526701e4a7dbd46afe828e846393f906990027
Jonas Bulcke <jonas.bulcke@student.ehb.be>
2021-09-22 13:11:14 +0200

    uploading 3D car

---
+++ src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Landing = () => {
+    return (
+        <div>
+            <div>
+                <button>
+                </button>
+            </div>
+            <div>
+                Welcome to the new and supersonic model of Volkswagen:
+                the <span>VW-Erasmus</span>! You can take a look yo our latest model by dragging the car around.
+            </div>
+        </div>
+    );
+}
