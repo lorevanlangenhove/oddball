a6ca1446ea5ae24971dea4de4ee742f58467e5ad
Lore Van Langenhove <lore.van.langenhove@student.ehb.be>
2021-09-22 13:05:32 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './car.css';
 import React, {Suspense} from "react";
 
 
@@ -7,15 +8,49 @@ import {Canvas} from "@react-three/fiber";
 import Loader from "./components/Loader";
 import {Environment, OrbitControls} from "@react-three/drei";
 
+function Test(){
+    return(
+        <div>
+            <div class="container">
+                <div class="row">
+                    <div class="col" id="three">
+                        <div>
+                            <button type="button">
+
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col" id="big">
+                        <h1>ERVAAR DE <br /> <span class="vw">VW-ERASMUS</span></h1>
+                    </div>
+                    <div class="col" id="logo">
+                        <img src="/src/dieteren.svg"></img>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col" id="auto">
+                        <Canvas>
+                            <Suspense fallback={<Loader/>}>
+                                <Scene/>
+                                <OrbitControls/>
+                            </Suspense>
+                        </Canvas>
+                    </div> 
+
+                    <div class="col" id="ex">
+                        Welcome to the new and <br /> supersonic model of <br /> Volkswagen: <br /> the <span>VW-ERASMUS</span>! You can <br /> take a look to our latest <br /> model by dragging the car <br /> around.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 function App() {
     return (
         <div className="app">
-            <Canvas>
-                <Suspense fallback={<Loader/>}>
-                    <Scene/>
-                    <OrbitControls/>
-                </Suspense>
-            </Canvas>
+            <Test />    
         </div>
     );
 }
---
+++ src/Test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import 'car.css';
+const Test = () => {
+    return(
+        <div>
+            <div class="big">
+                <h1>ERVAAR DE <span>VW-ERASMUS</span></h1>
+            </div>
+
+            <div>
+                <p>Welcome to the new and supersonic model of Volkswagen: the <span>VW-ERASMUS</span>! You can take a look to our latest model by dragging the car around.</p>
+            </div>
+    </div>
+    )
+}
