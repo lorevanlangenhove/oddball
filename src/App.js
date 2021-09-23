8b51bdea3841400cce05c00943f2cfc64e6130bd
Jonas Bulcke <jonas.bulcke@student.ehb.be>
2021-09-23 15:26:21 +0200

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,75 +1,30 @@
 import './App.css';
 import './car.css';
-import React, {Suspense, useState} from "react";
+import React from "react";
 
+import Car from "./components/Car";
 
-import Scene from "./components/Scene";
-import {Canvas} from "@react-three/fiber";
-import Loader from "./components/Loader";
-import {Environment, OrbitControls} from "@react-three/drei";
-
-function Test() {
-    const [isPlaying, setPlaying] = useState(false);
-    let carComponent;
-
-    if (!isPlaying) {
-        carComponent = (
-            <div className="row" id="two">
-                <div className="col" id="auto">
-                    <button type="button" id="car" onClick={() => setPlaying(true)}>
-                        <img src="green.png"/>
-                    </button>
-                </div>
-
-                <div className="col" id="ex">
-                    Welcome to the new and <br/> supersonic model
-                    of <br/> Volkswagen: <br/> the <span>VW-ERASMUS</span>! You can <br/> take a look to our
-                    latest <br/> model by dragging the car <br/> around.
-                </div>
-            </div>
-        );
-    } else {
-        carComponent = (
-            <div className="row" id="id">
-                <div className="sketchfab-embed-wrapper">
-                    <iframe width="100%" height="100%" title="Auto" frameBorder="0" allowFullScreen
-                            mozallowfullscreen="true" webkitallowfullscreen="true"
-                            allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking
-                            execution-while-out-of-viewport execution-while-not-rendered web-share
-                            src="https://sketchfab.com/models/1e54eddf120948fe9514c6bab9d1a232/embed?autostart=1&camera=0&preload=1"></iframe>
-                </div>
-                <div class="buttons">
-                    <button className="carbutton" id="testrit">
-                        TESTRIT
-                    </button>
-
-                    <button className="carbutton" id="bestelnu"> 
-                        BESTEL NU
-                    </button>
-                </div>
-            </div>
-        );
-    }
-
+function App() {
     return (
-        <div class="container">
-            <div class="row" id="three">
+        <div className="app">
+            <div className="container">
+                <div className="row" id="three">
 
-                <div class="col" id="logo">
-                    <div class="dieteren">
-                        <img src="logo1.png"/>
+                    <div className="col" id="logo">
+                        <div className="dieteren">
+                            <img src="logo1.png"/>
+                        </div>
+                    </div>
+
+                    <div className="col" id="big">
+                        <h1>ERVAAR DE <br/> <span className="vw">VW-ERASMUS</span></h1>
                     </div>
                 </div>
 
-                <div class="col" id="big">
-                    <h1>ERVAAR DE <br/> <span class="vw">VW-ERASMUS</span></h1>
-                </div>
-            </div>
-
-            {carComponent}
+                <Car/>
 
                 <footer>
-                    <div clas="row" id="foot">
+                    <div className="row" id="foot">
                         <div className="col" id="cr">
                             <p>&copy;D'Ieteren</p>
                         </div>
@@ -88,19 +43,12 @@ function Test() {
                             </a>
                         </div>
 
-                    <div className="col" id="adres">
-                        <p>Maliestraat, 50 - 1050 Brussel - België</p>
+                        <div className="col" id="adres">
+                            <p>Maliestraat, 50 - 1050 Brussel - België</p>
+                        </div>
                     </div>
-                </div>
-            </footer>
-        </div>
-    )
-}
-
-function App() {
-    return (
-        <div className="app">
-            <Test/>
+                </footer>
+            </div>
         </div>
     );
 }
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,23 +1,47 @@
-import * as THREE from "three";
-import {Canvas} from "@react-three/fiber";
-import {useLoader} from "@react-three/fiber";
-import {Environment, OrbitControls} from "@react-three/drei";
-import {OBJLoader} from "three/examples/jsm/loaders/OBJLoader";
-import React, {Suspense} from "react";
-import Loader from "./Loader";
-
-const auto = "/assets/auto.obj"
+import React, {useState} from "react";
 
 
 const Car = () => {
-    const obj = useLoader(OBJLoader, auto);
+    const [isPlaying, setPlaying] = useState(false);
 
-    console.log("obj:")
-    console.log(obj);
+    if (!isPlaying) {
+        return (
+            <div className="row" id="two">
+                <div className="col" id="auto">
+                    <button type="button" id="car" onClick={() => setPlaying(true)}>
+                        <img src="../green.png"/>
+                    </button>
+                </div>
 
-    return (
-        <primitive object={obj}/>
-    );
+                <div className="col" id="ex">
+                    Welcome to the new and <br/> supersonic model
+                    of <br/> Volkswagen: <br/> the <span>VW-ERASMUS</span>! You can <br/> take a look to our
+                    latest <br/> model by dragging the car <br/> around.
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="row" id="id">
+                <div className="sketchfab-embed-wrapper">
+                    <iframe width="100%" height="100%" title="Auto" frameBorder="0" allowFullScreen
+                            mozallowfullscreen="true" webkitallowfullscreen="true"
+                            allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking
+                            execution-while-out-of-viewport execution-while-not-rendered web-share
+                            src="https://sketchfab.com/models/1e54eddf120948fe9514c6bab9d1a232/embed?autostart=1&camera=0&preload=1"/>
+                </div>
+                <div class="buttons">
+                    <button className="carbutton" id="testrit">
+                        TESTRIT
+                    </button>
+
+                    <button className="carbutton" id="bestelnu">
+                        BESTEL NU
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Car;
